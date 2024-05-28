4b0a4a7101ee4083f44280b713abf0b47ca5a67e
harshulmathur <harshul.mathur@humanli.ai>
2024-05-28 11:08:09 +0530

    Delete src/app/enviroments directory

--- src/app/enviroments/enviroments.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCIWEE5gkm6BGBkBsbs4uX8Gr3XPZLIba8",
-    authDomain: "trip-planner-7748a.firebaseapp.com",
-    projectId: "trip-planner-7748a",
-    storageBucket: "trip-planner-7748a.appspot.com",
-    messagingSenderId: "368183428678",
-    appId: "1:368183428678:web:da27f95ca9ec5c38956c47",
-    measurementId: "G-NY1DNG7MB2"
-  };
